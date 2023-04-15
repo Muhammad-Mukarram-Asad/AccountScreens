48ce2956b958cdfd529c3b9a1f9fe75e4a13a63f
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-04-16 02:35:42 +0500

    Add files via upload

--- components/SecondScreen/HKAccountItem.js
+++ components/SecondScreen/HKAccountItem.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./HKAccountItem.css";
 import WhatYouhavebg from "../../public/icons/WhatYouhavebg.svg";
-import bankIcon from "../../public/icons/bankIcon.svg";
 import wallet from "../../public/icons/wallet.svg";
-// import plus from "../../public/icons/Add.svg";
-import {AiOutlinePlus} from "react-icons/ai";
-import { Link } from "react-router-dom";
+import info from "../../public/icons/infoIcon.svg";
+import LeftArrow from "../../public/icons/backArrowIcon.svg"
+import rightArrow from "../../public/icons/RightArrow.svg"
+
+import { AiOutlinePlus } from "react-icons/ai";
+
 import HKAccountListing2 from "./Inner_item/HKAccountListing";
 import HKBottomNav from "../Bottom_Nav/HKBottomNav";
+
+import { useNavigate } from "react-router-dom";
+
+// import bankIcon from "../../public/icons/bankIcon.svg";
 // import MorePopupWithContent from "../../HKMore/MorePopupWithContent/MorePopupWithContent";
+
 const HKAccountItem = (props) => {
   const {
-    currency,
-    amount,
     bgColor = "#4799a6",
-    accountName = "Alfalah Bank",
-    accountIcon,
     accountItemClick,
     moreClick,
-    index = -1,
     isAccountDeactivated = false,
   } = props;
 
@@ -31,24 +33,29 @@ const HKAccountItem = (props) => {
     borderRadius: "16px",
   };
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="title">
+      <div className="HK_AccountItem_title">
+        <img src={LeftArrow} alt="back_arrow" onClick={()=> {navigate(-1)}} />
         <h1> All Accounts</h1>
-        <hr id="hr" />
+        <img id="hide_right_arrow" src={rightArrow} alt="forward_arrow" />
+        
+        {/* <hr id="hr" /> */}
       </div>
 
       <div className="AccountItem-Container">
         <div id="custom_div" style={bgStyle}></div>
         <div className="AccountItem-overlay">
           <div className="mainGrid-account">
-            <div className="AccountItem-info-account-area">
-              <p onClick={() => accountItemClick(index)}>{accountName}</p>
-
+            {/* <div className="AccountItem-info-account-area">
               <div className="AccountItem-moreArea" onClick={moreClick}>
                 <img src={bankIcon} width={24} height={24} alt="bank_icon_image" />
               </div>
-            </div>
+
+            </div> */}
+
             <div className="AccountItem-available-to-spend-area">
               <p className="heading">Available to Spend</p>
               <div className="AccountItem-spend-amount">
@@ -56,10 +63,12 @@ const HKAccountItem = (props) => {
                 <p className="amount">20,000</p>
               </div>
             </div>
+
             <div className="AccountItem-account-icon-area">
+              <img src={info} alt="info_icon_img" />
               <div
                 className="AccountItem-account-icon-area-btn-main"
-                onClick={() => accountItemClick(index)}
+                onClick={accountItemClick}
               >
                 <img src={wallet} width={24} height={24} alt="wallet_image" />
               </div>
@@ -75,20 +84,23 @@ const HKAccountItem = (props) => {
 
         <div className="accounts_div">
           <h2> Your Accounts</h2>
-          <Link to="/addAccount">
-            <button id="accounts_btn">
-                <AiOutlinePlus id="plus_icon" />
+          <button
+            id="accounts_btn"
+            onClick={() => {
+              navigate("/addAccount");
+            }}
+          >
+            <AiOutlinePlus id="plus_icon" />
             ADD ACCOUNT
-            </button>
-          </Link>
+          </button>
         </div>
+        {
+          <div className="accountlistingdiv">
+            <HKAccountListing2 />
+          </div>
+        }
+        <HKBottomNav />
       </div>
-      {
-        <>
-          <HKAccountListing2 />
-          <HKBottomNav />
-        </>
-      }
     </>
   );
 };
