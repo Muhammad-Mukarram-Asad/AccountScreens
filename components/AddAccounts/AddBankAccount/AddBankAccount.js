77a24e19809d49e2296eb9caa694c9e3213f3272
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-04-19 17:13:51 +0500

    Add files via upload

--- components/AddAccounts/AddBankAccount/AddBankAccount.js
+++ components/AddAccounts/AddBankAccount/AddBankAccount.js
@@ -4,7 +4,7 @@ import AccountNegative from "../../../public/icons/AccOpeningNegative.svg";
 import BackArrow from "../../../public/icons/backArrowIcon.svg";
 import RightArrow from "../../../public/icons/RightArrow.svg";
 import BankIcon from "../../../public/icons/bankIcon.svg"
-import "./AddBankAccount.css";
+import styles from "./AddBankAccount.module.css";
 
 import { useNavigate } from "react-router-dom";
 
@@ -12,33 +12,50 @@ import { useNavigate } from "react-router-dom";
 function AddBankAccount() {
   const navigate = useNavigate();
   return (
-    <div className="add_bank_account_main_div">
-      <div id="main_heading_div">
-        <img src={BackArrow} alt="back_arrow_image" />
+    <div className={styles.add_bank_account_main_div}>
+      <div className={styles.main_heading_div}>
+        <img src={BackArrow} alt="back_arrow_image" onClick={()=> {navigate(-1)}} />
         <h1>Add Bank Account</h1>
-        <img src={RightArrow} alt="back_arrow_image" />
+        <img src={RightArrow} className={styles.right_arrow_icon} alt="back_arrow_image" />
       </div>
 
-      <div id="horizontal_line"></div>
+      <div className={styles.horizontal_line}></div>
 
-      <div className="account_title">
+      <div className={styles.account_title}>
         <h1>Enter an account title</h1>
         <p> Title should be longer than 3 and less than 50 characters</p>
-        <div className="account_title_inner_div">
+        <div className={styles.account_title_inner_div}>
           <img src={BankIcon} alt="bank_icon" />
-          <input type="text" placeholder="Bank Alfalah" maxLength={10} />
+          <input type="text" placeholder="Bank Alfalah" maxLength={30} />
         </div>
       </div>
 
-
-      <div className="account_balance">
-        <h1>Opening Balance</h1>
-        <p> How much money do you already have in this account?</p>
-        <input type="text" placeholder="PKR 0" minLength={4} />
+      <div className={styles.account_amount}>
+        <h1>Last 4 Digits of Bank Number (Optional)</h1>
+        <input type="number" placeholder="3567" minLength={4} />
       </div>
 
-      <div className="account_opening_div">
-        <div className="add_accounts_inner_div" id="left_side" onClick={()=> {navigate("/selectAccount")}}>
+
+      <div className={styles.account_balance}>
+        <h1>Opening Balance</h1>
+        <p> How much money do you already have in this account?</p>
+        <div className={styles.account_balance_selection_currency}>
+        <select>
+          <option selected>PKR</option>
+          <option>USD</option>
+          <option>INR</option>
+          <option>Euro</option>
+          <option>Pound</option>
+          <option>Riyal</option>
+          </select>        
+        <input type="text" placeholder="PKR 0" minLength={4} />
+
+        </div>
+        <p className={styles.amount_warning_p}>Amount Cannot be negative</p>
+      </div>
+
+      <div className={styles.account_opening_div}>
+        <div className={`${styles.add_accounts_inner_div} ${styles.left_side}`} onClick={()=> {navigate("/selectAccount")}}>
             <img src={AccountPositive} alt="acc_positive" />
             <h5> Positive</h5>
             <p>
@@ -46,7 +63,7 @@ function AddBankAccount() {
             </p>
         </div>
 
-        <div className="add_accounts_inner_div" id='right_side' onClick={()=> {navigate("/selectAccount")}}>
+        <div className={`${styles.add_accounts_inner_div} ${styles.right_side}`} onClick={()=> {navigate("/selectAccount")}}>
             <img src={AccountNegative} alt="acc_positive" />
             <h5>Negative</h5>
             <p>
@@ -55,7 +72,7 @@ function AddBankAccount() {
         </div>
       </div>
 
-      <button id="add_account_btn">Add Bank Account</button>
+      <button className={styles.add_account_btn}>Add Bank Account</button>
 
     </div>
   );
