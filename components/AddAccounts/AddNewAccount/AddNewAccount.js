cd1bdf6fd4f2eac9d6e7dd86a4f0a0b82fe7e283
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-04-20 22:15:04 +0500

    Add files via upload

--- components/AddAccounts/AddNewAccount/AddNewAccount.js
+++ components/AddAccounts/AddNewAccount/AddNewAccount.js
@@ -9,50 +9,50 @@ import RightArrow from "../../../public/icons/RightArrow.svg";
 
 import { useNavigate } from "react-router-dom";
 
-import "./AddNewAccount.css";
+import styles from "./AddNewAccount.module.css";
 
 function AddNewAccount() {
   const navigate = useNavigate();
   return (
-    <div className="add_new_account_main_div">
-      <div id="main_heading_div">
-        <img src={BackArrow} alt="back_arrow_image" />
+    <div className={styles.add_new_account_main_div}>
+      <div className={styles.main_heading_div}>
+        <img src={BackArrow} alt="back_arrow_image" onClick={()=> {navigate(-1)}} />
         <h1>Add New Account</h1>
-        <img src={RightArrow} alt="back_arrow_image" />
+        <img src={RightArrow} className={styles.right_arrow_icon} alt="back_arrow_image" />
       </div>
 
-      <div id="horizontal_line"></div>
+      <hr className={styles.hr} />
 
-      <div className="selecting_account_div">
+      <div className={styles.selecting_account_div}>
         <p>Choose your accoun type</p>
 
-        <div className="accounts_list">
-          <div className="account_list_inner_div" onClick={()=>{navigate("/addCashAccount")}}>
-              <div className="account_list_img_div">
+        <div className={styles.accounts_list}>
+          <div className={styles.account_list_inner_div} onClick={()=>{navigate("/addCashAccount")}}>
+              <div className={styles.account_list_img_div}>
                 <img src={Cash} alt="cash_icon_img" />
               </div>
               <p>Cash</p>
           </div>
 
-          <hr id="hr" />
+          <hr className={styles.hr} />
 
-          <div className="account_list_inner_div" onClick={()=>{navigate("/addBankAccount")}}>
-              <div className="account_list_img_div">
+          <div className={styles.account_list_inner_div} onClick={()=>{navigate("/addBankAccount")}}>
+              <div className={styles.account_list_img_div}>
                 <img src={Bank_Building} alt="Bank_building_icon_img" />
               </div>
               <p>Bank</p>
           </div>
 
-          <hr id="hr" />
+          <hr className={styles.hr} />
 
-          <div className="account_list_inner_div" onClick={()=>{navigate("/addPersonAccount")}}>
-              <div className="account_list_img_div">
+          <div className={styles.account_list_inner_div} onClick={()=>{navigate("/addPersonAccount")}}>
+              <div className={styles.account_list_img_div}>
                 <img src={Add_Person} alt="person_icon_img" />
               </div>
               <p>Person</p>
           </div>
 
-          <hr id="hr" />
+          <hr className={styles.hr} />
         </div>
       </div>
     </div>
