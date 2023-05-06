f39f6ff90df98ceae8adf9eb9bb8e604f76be576
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-05-07 02:47:34 +0500

    Add files via upload

--- components/AddAccounts/CashAccount/Cash_Account.js
+++ components/AddAccounts/CashAccount/Cash_Account.js
@@ -10,26 +10,42 @@ function CashAccount() {
   return (
     <div className={styles.cash_account_main_div}>
       <div className={styles.main_heading_div}>
-        <img src={BackArrow} alt="back_arrow_image" onClick={()=> {navigate(-1)}} />
+        <img
+          src={BackArrow}
+          alt="back_arrow_image"
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
         <h1>Add Cash Account</h1>
-        <img src={RightArrow} className={styles.right_arrow_icon} alt="back_arrow_image" />
+        <img
+          src={RightArrow}
+          className={styles.right_arrow_icon}
+          alt="back_arrow_image"
+        />
       </div>
 
-      <div className={styles.horizontal_line}></div>
+      {/* <div className={styles.horizontal_line}></div> */}
 
       <div className={styles.cash_account_balance}>
         <h1>Account title</h1>
-        <input
-          type="text"
-          placeholder="My Cash Account"
-          maxLength={50}
-        />
+        <input type="text" placeholder="My Cash Account" maxLength={50} />
       </div>
 
       <div className={styles.cash_account_balance}>
         <h1>Opening Balance</h1>
         <p className={styles.cash_p}> How much cash do you have?</p>
-        <input type="text" placeholder="PKR 0" minLength={4} />
+        <div className={styles.account_balance_selection_currency}>
+          <select>
+            <option defaultValue>PKR</option>
+            <option>USD</option>
+            <option>INR</option>
+            <option>Euro</option>
+            <option>Pound</option>
+            <option>Riyal</option>
+          </select>
+          <input type="text" placeholder="PKR 0" minLength={4} />
+        </div>
       </div>
 
       <button className={styles.add_cash_btn}>Add Cash Account</button>
